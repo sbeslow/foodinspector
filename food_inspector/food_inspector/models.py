72c33b72418e50a37541f2f5acd98b373dd7f32f
sbeslow <bezzy21@yahoo.com>
2015-12-28 20:49:59 -0600

    Added restaurant to the model

--- food_inspector/food_inspector/models.py
+++ food_inspector/food_inspector/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Search(models.Model):
     searched_term = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now=True)
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=35)
+    zip_code = models.CharField(max_length=10)
