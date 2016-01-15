be8099f9825db8a4879ea1824f39f5e8f6cefd13
sbeslow <bezzy21@yahoo.com>
2016-01-14 20:52:48 -0600

    Doing a bit of sorting of the database results

--- food_inspector/food_inspector/restaurant_finder.py
+++ food_inspector/food_inspector/restaurant_finder.py
@@ -1,25 +1,66 @@
 from django.db import connection
+import re
 
 
 def find_restaurant_in_db(restaurant_name):
+
+    restaurants_from_db = likesounding_restaurants(restaurant_name)
+
+    restaurant_trimmed = trim_name_for_stop_words(restaurant_name)
+    print("Trimmed name is %s" % restaurant_trimmed)
+    buckets = {"1": [], "2": [], "3": []}
+    for iter_restaurant in restaurants_from_db:
+        iter_restaurant_trimmed = trim_name_for_stop_words(iter_restaurant["name"])
+        print("Checking db restaurant %s" % restaurant_trimmed + " in " + iter_restaurant_trimmed)
+        if restaurant_trimmed in iter_restaurant_trimmed:
+            buckets["1"].append(iter_restaurant)
+            print(iter_restaurant["name"] + " added to bucket 1")
+        else:
+            added = False
+            for word_in_name in restaurant_trimmed.split(" "):
+                if word_in_name in iter_restaurant_trimmed:
+                    buckets["2"].append(iter_restaurant)
+                    print(iter_restaurant["name"]  + " added to bucket 2")
+                    added = True
+                    break
+            if added is False:
+                buckets["3"].append(iter_restaurant)
+                print(iter_restaurant["name"]  + " added to bucket 3")
+
+
+    return buckets["1"] + buckets["2"] + buckets["3"]
+
+
+def likesounding_restaurants(restaurant_name):
     cursor = connection.cursor()
     print("RESTAURANT IS: " + restaurant_name)
 
-    query = "SELECT *"
-    query += ",difference(food_inspector_restaurant.name,'%s') as diff, " % restaurant_name
-    query += "levenshtein(food_inspector_restaurant.name, '%s') as lev " % restaurant_name
-    query += "from food_inspector_restaurant where soundex(name) = soundex('%s') " % restaurant_name
-    query += "order by diff, lev desc;"
+    # Forget about PEP8 for the next few lines
+    query = "SELECT *, " +\
+            "difference(food_inspector_restaurant.name,'%s') as diff, " % restaurant_name +\
+            "levenshtein(food_inspector_restaurant.name, '%s') as lev " % restaurant_name +\
+            "from food_inspector_restaurant where soundex(name) = soundex('%s') " % restaurant_name +\
+            "order by diff, lev desc;"
 
-    print(query)
+    print("Searching database with: %s" % query)
 
     cursor.execute(query)
     results = dictfetchall(cursor)
     return results
 
 
+def trim_name_for_stop_words(restaurant_name):
+    stop_words = [' the ', ' a ', ' and ', '&', '\'', ]
+
+    clean_restaurant_name = re.sub('[^a-zA-Z0-9 \n\.]', ' ', restaurant_name.replace('\'', ''))
+
+    trimmed_name = ''
+    for word in clean_restaurant_name.split(" "):
+        if word not in stop_words:
+            trimmed_name += ' ' + word
+    return trimmed_name.strip().upper()
+
 def dictfetchall(cursor):
-    "Return all rows from a cursor as a dict"
     columns = [col[0] for col in cursor.description]
     return [
         dict(zip(columns, row))
